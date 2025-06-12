6aa79d3d92c789828754bf92fc95ec7644d4485a
Fernando Arias Marqués <ferarias@gmail.com>
2025-06-12 20:14:11 +0200

    Add HomeAssistantBinarySensorAttribute

---
+++ src/Samqtt.Common/SystemSensors/HomeAssistantBinarySensorAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Samqtt.SystemSensors
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class HomeAssistantBinarySensorAttribute(string? payloadOn = "on", string? payloadOff = "off", string? deviceClass = null) : Attribute
+    {
+        public string? PayloadOn { get; } = payloadOn;
+        public string? DeviceClass { get; } = deviceClass;
+        public string? PayLoadOff { get; } = payloadOff;
+    }
+}
