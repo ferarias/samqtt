ff534de9bed2fba1de2e35fa2b11102612e2b33d
Fernando Arias Marqués <ferarias@gmail.com>
2025-06-20 16:29:55 +0200

    move TopicProvider to Application

---
+++ src/Samqtt.Application/TopicProvider.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Options;
+using MQTTnet.Extensions.TopicTemplate;
+using Samqtt.Common;
+using Samqtt.Options;
+
+namespace Samqtt.Application
+{
+    public class TopicProvider(IOptions<SamqttOptions> options) : ITopicProvider
+    {
+        public static readonly string AppIdTopic = Constants.AppId.ToLowerInvariant();
+
+        public string DeviceIdentifier => options.Value.DeviceIdentifier;
+        public string BaseTopic { get; } = $"{AppIdTopic}/{options.Value.DeviceIdentifier}";
+        public string StatusTopic { get; } = $"{AppIdTopic}/{options.Value.DeviceIdentifier}/status";
+        public string UnsubscribeTopic { get; } = $"{AppIdTopic}/{options.Value.DeviceIdentifier}/#";
+
+        public string GetUniqueId(string name) => $"{AppIdTopic}_{DeviceIdentifier}_{SanitizeHelpers.Sanitize(name)}";
+        public string GetStateTopic(string name) => $"{AppIdTopic}/{options.Value.DeviceIdentifier}/{SanitizeHelpers.Sanitize(name)}";
+    }
+
+    public static class MqttTopicTemplates
+    {
+        public static readonly MqttTopicTemplate SensorState =
+            new("samqtt/{device}/sensor/{sensor}/state");
+
+        public static readonly MqttTopicTemplate ActionCommand =
+            new("samqtt/{device}/switch/{action}/command");
+
+        public static readonly MqttTopicTemplate Status =
+            new("samqtt/{device}/status");
+
+    }
+
+}
--- src/Samqtt.Common/ITopicProvider.cs
+++ src/Samqtt.Common/ITopicProvider.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.Extensions.Options;
-using MQTTnet.Extensions.TopicTemplate;
-using Samqtt.Common;
-using Samqtt.Options;
+﻿using MQTTnet.Extensions.TopicTemplate;
 
 namespace Samqtt
 {
@@ -16,29 +13,4 @@ namespace Samqtt
         public string GetStateTopic(string name);
     }
 
-    public class TopicProvider(IOptions<SamqttOptions> options) : ITopicProvider
-    {
-        public static readonly string AppIdTopic = Constants.AppId.ToLowerInvariant();
-
-        public string DeviceIdentifier => options.Value.DeviceIdentifier;
-        public string BaseTopic { get; } = $"{AppIdTopic}/{options.Value.DeviceIdentifier}";
-        public string StatusTopic { get; } = $"{AppIdTopic}/{options.Value.DeviceIdentifier}/status";
-        public string UnsubscribeTopic { get; } = $"{AppIdTopic}/{options.Value.DeviceIdentifier}/#";
-
-        public string GetUniqueId(string name) => $"{AppIdTopic}_{DeviceIdentifier}_{SanitizeHelpers.Sanitize(name)}";
-        public string GetStateTopic(string name) => $"{AppIdTopic}/{options.Value.DeviceIdentifier}/{SanitizeHelpers.Sanitize(name)}";
-    }
-
-    public static class MqttTopicTemplates
-    {
-        public static readonly MqttTopicTemplate SensorState =
-            new("samqtt/{device}/sensor/{sensor}/state");
-
-        public static readonly MqttTopicTemplate ActionCommand =
-            new("samqtt/{device}/switch/{action}/command");
-
-        public static readonly MqttTopicTemplate Status =
-            new("samqtt/{device}/status");
-
-    }
 }
